f660a11dd970a4b2bdc1a6a914e07c8b05b9d702
JuYoung Lee <duddud931215@gmail.com>
2025-06-17 18:38:33 +0900

    feat: [이주영] home-app 기능구현

--- home-app/src/App.tsx
+++ home-app/src/App.tsx
@@ -2,12 +2,21 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 import "./index.css";
+import GlobalStyle from "./styles/GlobalStyle.ts";
+import Section1 from "./component/Section1.tsx";
+import Section2 from "./component/Section2.tsx";
+import Section3 from "./component/Section3.tsx";
 
 const App = () => {
+
   return (
-  <div>
-    머라도 나와라
-  </div>
+      <>
+        <GlobalStyle />
+        <Section1 />
+        <Section2 />
+        <Section3 />
+      </>
+
   );
 };
 
