0956f2f3e7fef883de79acb3c7559ebb642e9754
JuYoung Lee <duddud931215@gmail.com>
2025-06-18 19:29:14 +0900

    add:[이주영] home-app 내용 추가

--- home-app/src/component/Section5.tsx
+++ home-app/src/component/Section5.tsx
@@ -59,7 +59,7 @@ const Section5 = () => {
                 <Description>“전문가처럼 과학적으로 투자할 순 없을까?”</Description>
                 <br/>
                 <Description>4getMoney가 시뮬레이션 결과를 분석해 알려드립니다.</Description>
-                <Description>포트폴리오에 숨어있는 위험성을 더리치 실험실에서 확인해보세요.</Description>
+                <Description>포트폴리오에 숨어있는 위험성을 4getMoney 실험실에서 확인해보세요.</Description>
             </LeftSection>
             <RightSection>
                <img src={Sec5_image} alt="Section5 이미지"/>
